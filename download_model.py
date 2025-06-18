31d3fd39787a975c801f69eb952d5d40acb3f29b
pelegc49 <pelegc49@gmail.com>
2025-06-18 15:52:33 +0300

    Uncomment model download code for KBlueLeaf/kohaku-v2.1

--- download_model.py
+++ download_model.py
@@ -1,11 +1,11 @@
 from huggingface_hub import snapshot_download
 
 # Download the model
-# model_path = snapshot_download(
-#     "KBlueLeaf/kohaku-v2.1",
-#     local_dir="models/Model/kohaku-v2.1",
-#     local_dir_use_symlinks=False
-# )
+model_path = snapshot_download(
+    "KBlueLeaf/kohaku-v2.1",
+    local_dir="models/Model/kohaku-v2.1",
+    local_dir_use_symlinks=False
+)
 # model_path = snapshot_download(
 #     "mirroring/pastel-mix",
 #     local_dir="models/Model/pastel-mix",
